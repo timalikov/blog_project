76b50138c48e4cf73559def24e6b053c072b1da6
Temirlan <malikovtemirlan4@gmail.com>
2023-10-19 17:47:42 +0600

    DRY principle, made functions global

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -5,48 +5,11 @@ from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework import serializers
+from blog_project.utils import handle_get_request, handle_post_request, handle_update_request, handle_delete_request
+
 
 from .serializers import *
 
-
-#Universal functions to handle the different requests
-def handle_get_request(model, serializer, query_params=None):
-    objects = model.objects.filter(**query_params) if query_params else model.objects.all()
-    if objects:
-        serialized_objects = serializer(objects, many=True)
-        return Response(serialized_objects.data)
-    else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-def handle_post_request(serializer_class, data):
-    serializer = serializer_class(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    
-def handle_update_request(model, serializer_class, pk, data):
-    try:
-        instance = model.objects.get(pk=pk)
-    except ObjectDoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    serializer = serializer_class(instance=instance, data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    
-def handle_delete_request(model, pk):
-    try:
-        instance = model.objects.get(pk=pk)
-    except ObjectDoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    instance.delete()
-    return Response(status=status.HTTP_202_ACCEPTED)
 
 #Overview of endpoints
 @api_view(['GET'])
